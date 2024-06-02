5d1a531b6ce7c6b26bd3da9ea6bcb2f6a03ac123
MohammadMosleh <mohammadnmosleh123@gmail.com>
2024-06-02 18:01:00 +0300

    first commit

---
+++ src/main/java/com/example/HotelManagementSystem/HotelManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.HotelManagementSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelManagementSystemApplication.class, args);
+	}
+
+}
